ee3b0aa14eec6ee82438064bd5c1aa94886f36df
Iftee1711402 <tawsifhaque97@gmail.com>
2022-05-16 20:57:12 +0600

    ...

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=57fa5073
 
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '57fa5073',
-            s: 'avengers'
+            s: searchTerm
         }
     });
-    console.log(response.data.Search);
+    console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = (event) => {
+    // make searches after 1 second of no typing
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 1000);
+};
+
+input.addEventListener('input', onInput);
+
+// debouncing an input: waiting for some time to pass after the last event to actually do something
