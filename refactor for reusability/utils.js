0e3b9b78fffd195415033005f455035df34fb4d0
Iftee1711402 <tawsifhaque97@gmail.com>
2022-05-18 05:19:36 +0600

    added directory for refactored reusabile code

---
+++ refactor for reusability/utils.js
@@ -0,0 +1,12 @@
+// // debouncing an input: waiting for some time to pass after the last event to actually do something
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
